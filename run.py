acce2e73b5a3aab2436e3849b448e804f87550ca
gatilin <2267379130@qq.com>
2024-08-25 18:45:58 +0800

    update tokenizer.py

--- run.py
+++ run.py
@@ -7,12 +7,13 @@ from tokenizer.regex_tokenizer import TextChunker
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Chunk text file.')
     parser.add_argument('file_path', type=str, help='Path to the text file')
-    parser.add_argument('output_file', type=str, help='Path to the output JSON file', nargs='?', default='output.json')
+    parser.add_argument('output_file', type=str, help='Path to the output JSONL file', nargs='?', default='output.jsonl')
+    parser.add_argument('--config', type=str, help='Path to the YAML config file', default='config.yaml')
     args = parser.parse_args()
 
     if not os.path.exists(args.file_path):
         print(f"File not found: {args.file_path}")
         sys.exit(1)
 
-    chunker = TextChunker(args.output_file)
+    chunker = TextChunker(config_file=args.config, output_file=args.output_file)
     chunker.process_text(args.file_path)
